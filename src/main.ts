486191d3b6aee1982ddecb292cdefb2057bbed31
JEspinosa1770 <jespinosa1770@gmail.com>
2026-03-17 21:30:28 +0100

    chore(deploy): deploy to Render and update configuration

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,8 @@ async function bootstrap() {
 
   app.enableCors({
     origin: [
-      'http://localhost:4200',
+      'http://smart-travel-planner-web.vercel.app',
+      // 'http://localhost:4200',
       // añadir las URLs de producción cuando estén
     ],
   });
